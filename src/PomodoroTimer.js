3afc388cd167e8c3604b114e66c33d056783d2f7
Michael The <mcgthe@gmail.com>
2018-10-16 17:08:01 +0200

    Add About page

--- src/PomodoroTimer.js
+++ src/PomodoroTimer.js
@@ -60,18 +60,18 @@ function SettingsModal(props) {
                             </div>
 
                             <div className="form-row">
-                            <div className="form-group col">
-                                <label htmlFor="settings-pomodoro">Pomodoro</label>
-                                <input id="settings-pomodoro" name="pomodoro" className="form-control" type="number" min="1" max="60" value={props.durations.pomodoro / 60} onChange={(e) => props.handleTimeChange(e)}/>
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="settings-short-break">Short break</label>
-                                <input id="settings-short-break" name="shortBreak" className="form-control" type="number" min="1" max="60" value={props.durations.shortBreak / 60} onChange={(e) => props.handleTimeChange(e)}/>
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="settings-long-break">Long break</label>
-                                <input id="settings-long-break" name="longBreak" className="form-control" type="number" min="1" max="60" value={props.durations.longBreak / 60} onChange={(e) => props.handleTimeChange(e)}/>
-                            </div>
+                                <div className="form-group col">
+                                    <label htmlFor="settings-pomodoro">Pomodoro</label>
+                                    <input id="settings-pomodoro" name="pomodoro" className="form-control" type="number" min="1" max="60" value={props.durations.pomodoro / 60} onChange={(e) => props.handleTimeChange(e)}/>
+                                </div>
+                                <div className="form-group col">
+                                    <label htmlFor="settings-short-break">Short break</label>
+                                    <input id="settings-short-break" name="shortBreak" className="form-control" type="number" min="1" max="60" value={props.durations.shortBreak / 60} onChange={(e) => props.handleTimeChange(e)}/>
+                                </div>
+                                <div className="form-group col">
+                                    <label htmlFor="settings-long-break">Long break</label>
+                                    <input id="settings-long-break" name="longBreak" className="form-control" type="number" min="1" max="60" value={props.durations.longBreak / 60} onChange={(e) => props.handleTimeChange(e)}/>
+                                </div>
                             </div>
                         </form>
                     </div>
@@ -91,6 +91,44 @@ function LogModal(props) {
                     </div>
                     <div className="modal-content">
                         <p>Placeholder text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function AboutCard(props) {
+    return (
+        <div className="row">
+            <div className="col-sm-6 mx-auto">
+                <div className="card" id="about-card">
+                    <div className="card-body text-left">
+                        <h4 className="card-title">About</h4>
+                        <p className="card-text">
+                            The Pomodoro technique is a time management technique to work and study more productively.
+                            For more information, check out <a href="https://francescocirillo.com/pages/pomodoro-technique">this page</a>.
+                        </p>
+
+                        <h4 className="card-title">How does it work?</h4>
+                        <p>
+                            <ol>
+                                <li>Choose the task to perform</li>
+                                <li>Start the Pomodoro timer</li>
+                                <li>Work on the task until the timer rings</li>
+                                <li>Take a short break. Congratulations, you've completed a pomodoro!</li>
+                                <li>Every 4 pomodoros, take a longer break</li>
+                            </ol>
+                        </p>
+
+                        <h4>Why does it work?</h4>
+                        <p>Distractions are all around us. Right when we're in the middle of some focused work or study, we might get interrupted by an instant message, an e-mail, or a colleague dropping by. The goal of the Pomodoro technique is to limit the impact of these distractions. Try to stay focused during the whole session and postpone your distractions until later. See an e-mail coming in? You can look at it after finishing your pomodoro. Does a colleague want to grab coffee? Ask if they can wait until your break starts.</p>
+
+                        <h4>What's a pomodoro?</h4>
+                        <p>"Pomodoro" means "tomato" in Italian. The inventor of the Pomodoro technique, Francesco Cirillo, named the technique after the tomato-shaped timer from his university days.</p>
+
+                        <h4>About Tomatimely</h4>
+                        <p>Tomatimely is developed by <a href="http://www.michaelthe.com">Michael The</a> and the result of trying out countless Pomodoro timer apps and websites and feeling unsatisfied by all of them. They would all miss some feature the other would have. Tomatimely is made perfect for me — and maybe you'll find it useful as well.</p>
                     </div>
                 </div>
             </div>
@@ -248,6 +286,9 @@ class PomodoroTimer extends React.Component {
                     <LogModal
                         logs={this.props.logs}
                     />
+                </div>
+                <div className="my-4">
+                    <AboutCard />
                 </div>
             </div>
         );
